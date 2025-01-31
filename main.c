b3e6bb5e000f97710f8bd07d0c71b950dec49730
artemooooooooon <it.artemon@gmail.com>
2025-01-31 21:27:03 +0100

    02 zmiana mnożenia w metodzie Policz

--- main.c
+++ main.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
+int Policz(int a, int b) {
+    return a * b;
+}
+
 int main(void) {
     printf("Hello, World!\n");
+    printf("W drugim branchu; 2*3=%d",Policz(2,3));
     return 0;
 }
